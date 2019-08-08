2ca205495ec36c846138625eb3f966d63b417859
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2019-08-08 18:03:47 -0300

    Navegacao prev/next finalizada

--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,6 @@
 import debounce from './debounce.js';
 
-export default class Slide {
+export class Slide {
   constructor(slide, wrapper) {
       this.slide = document.querySelector(slide);
       this.wrapper = document.querySelector(wrapper);
@@ -165,6 +165,7 @@ export default class Slide {
 
 
   activePrevSlide() {
+    console.log(this.index);
     if (this.index.prev !== false)
       this.changeSlide(this.index.prev);
   }
@@ -179,6 +180,8 @@ export default class Slide {
     this.onStart = this.onStart.bind(this);
     this.onMove = this.onMove.bind(this);
     this.onEnd = this.onEnd.bind(this);
+    this.activePrevSlide = this.activePrevSlide.bind(this);
+    this.activeNextSlide = this.activeNextSlide.bind(this);
 
     this.onResize = debounce(this.onResize.bind(this), 200);
   }
@@ -191,6 +194,21 @@ export default class Slide {
     this.addResizeEvent();
     this.changeSlide(0);
 
+    return this;
   }
 
+}
+
+
+export class SlideNav extends Slide {
+  addArrow(prev, next) {
+    this.prevElement = document.querySelector(prev);
+    this.nextElement = document.querySelector(next);
+    this.addArrowEvent();
+  }
+
+  addArrowEvent() {
+    this.prevElement.addEventListener('click', this.activePrevSlide);
+    this.nextElement.addEventListener('click', this.activeNextSlide);
+  }
 }
